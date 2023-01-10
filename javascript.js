17aab49fe545127ea503f5640a3aa5757011f293
Matt DeNero <mdenero87@gmail.com>
2023-01-10 03:42:25 -0500

    Project complete

--- javascript.js
+++ javascript.js
@@ -1,52 +1,10 @@
-// Find all the cells and the reset button
-const cells = document.querySelectorAll("td");
-const resetButton = document.querySelector("#reset-button");
+(() => {
 
-// Set the current player to "X"
-let currentPlayer = "X";
 
-// Add a click event to each cell
-for (const cell of cells) {
-  cell.addEventListener("click", handleClick);
-}
-
-// Add a click event to the reset button
-resetButton.addEventListener("click", resetGame);
-
-// This function is called when a cell is clicked
-function handleClick() {
-  // Return if the cell has already been clicked
-  if (this.textContent) {
-    return;
-  }
-
-  // Mark the cell with the current player's symbol
-  this.textContent = currentPlayer;
-
-  // Check for a win
-  const winner = checkForWin();
-
-  // If there is a win, show an alert and reset the game
-  if (winner) {
-    alert(`Player ${winner} wins!`);
-    resetGame();
-    return;
-  }
-
-  // Check for a draw
-  if (checkForDraw()) {
-    // If there is a draw, show an alert and reset the game
-    alert("Draw!");
-    resetGame();
-    return;
-  }
-
-  // Switch to the other player
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-}
-
-// Check for a win by checking all possible combinations of three cells
-function checkForWin() {
+  // this will set the current player to "X"
+  let currentPlayer = "X";
+  
+  // this is an array of winning combinations
   const winningCombinations = [
     [1, 2, 3],
     [4, 5, 6],
@@ -57,48 +15,132 @@ function checkForWin() {
     [1, 5, 9],
     [3, 5, 7],
   ];
+  
+  // Array of cell elements
+  const cells = [
+    document.querySelector("#cell-1"),
+    document.querySelector("#cell-2"),
+    document.querySelector("#cell-3"),
+    document.querySelector("#cell-4"),
+    document.querySelector("#cell-5"),
+    document.querySelector("#cell-6"),
+    document.querySelector("#cell-7"),
+    document.querySelector("#cell-8"),
+    document.querySelector("#cell-9"),
+  ];
+  
+  // Add a click event to the start button
+  document.querySelector("#start-button").addEventListener("click", startGame);
+  
+  // Add a click event to the reset button
+  document.querySelector("#reset-button").addEventListener("click", resetGame);
 
-  // Check each combination of cells to see if they have the same symbol as the current player
-  for (const combination of winningCombinations) {
-    const [a, b, c] = combination;
-    if (
-      cells[a - 1].textContent === currentPlayer &&
-      cells[b - 1].textContent === currentPlayer &&
-      cells[c - 1].textContent === currentPlayer
-    ) {
-      return currentPlayer;
+
+  
+  //Starting the game
+  
+  // When a cell on the board is clieked this function will start using .textContent
+  function play() {
+    if (this.textContent) {
+      return;
     }
-  }
-
-  // If no combination of cells has the same symbol as the current player, return null
-  return null;
-}
-
-// Check for a draw by seeing if there are any empty cells
-function checkForDraw() {
-  for (const cell of cells) {
-    if (!cell.textContent) {
-      return false;
+  
+    // Mark the cell with the current player's X or O
+    this.textContent = currentPlayer;
+  
+  
+  
+  
+  
+    // Checks for a win
+    const winner = checkForWin();
+  
+    // If there is a win, show an alert and reset the game
+    if (winner) {
+      //alert.style.backgroundColor = "rgba(255, 0, 0, 0.4)";
+      alert("Player " + winner + " wins! Pizza and Cookies for you!!");
+      resetGame();
+      return;
     }
+  
+    // Checks for a draw
+    if (checkForDraw()) {
+      // If there is a draw, show an alert and reset the game
+      alert("Draw!  To bad...no pizza for anyone...");
+      resetGame();
+      return;
+    }
+  
+    // Switches to the other player
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
+
+    
+
+  }
+  
+
+  
+  // This will Ccheck for a win by checking all possible combinations of the cells on the board
+  
+  
+  
+  function checkForWin() {
+    // This gets the values of the cells in each combination using .map
+    const values = winningCombinations.map(function (combination) {
+      const [cell1, cell2, cell3] = combination.map(function (index) {
+        return cells[index - 1].innerHTML;
+      });
+      return [cell1, cell2, cell3];
+    });
+  
+    // Checks each combination for a win and if the player has marked 3 cells in a row return X or O
+    for (let i = 0; i < values.length; i++) {
+      const [cell1, cell2, cell3] = values[i];
+      if (cell1 === currentPlayer && cell2 === currentPlayer && cell3 === currentPlayer) {
+        return currentPlayer;
+      }
+    }
+  
+    // If there are no combinations it will return null
+    return null;
+  }
+  
+  //Looks for a draw by seeing if there are any empty cells
+  function checkForDraw() {
+    for (let i = 0; i < cells.length; i++) {
+      if (!cells[i].textContent) {
+        return false;
+      }
+    }
+  
+    // If there are no empty cells, return true
+    return true;
+  }
+  
+  // Reset the game by clearing all the cells and setting the current player to "X"
+  function resetGame() {
+    cells.forEach(function (cell) {
+      cell.textContent = "";
+    });
+  
+    currentPlayer = "X";
+  }
+  
+  // This function is the start button when clicked
+  function startGame() {
+    // Add a click event to each cell
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].addEventListener("click", play);
+    }
+  
+    // Hide the start button
+    document.querySelector("#start-button").style.display = "none";
   }
 
-  // If there are no empty cells, return true
-  return true;
-}
 
-// Reset the game by clearing all the cells and setting the current player to "X"
-function resetGame() {
-  for (const cell of cells) {
-    cell.textContent = "";
-  }
-
-  currentPlayer = "X";
-}
-
-
-
-
-
-
-
-
+  
+})();
